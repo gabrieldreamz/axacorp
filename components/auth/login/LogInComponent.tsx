43932e17dab46734b78f89f66e2202dcd0f3e5f2
Victor Mbah <mbahvictor16@gmail.com>
2024-05-27 10:06:11 +0100

    corrected the styling in the login page

--- components/auth/login/LogInComponent.tsx
+++ components/auth/login/LogInComponent.tsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 
 export default function LogInComponent() {
   return (
-    <section className="flex justify-center items-center">
+    <section className="flex justify-evenly items-center">
       <Form />
       <div className="hidden lg:block">
         <RegisterIcon />
