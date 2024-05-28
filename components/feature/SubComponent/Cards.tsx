171319dd571b3586855612008d628b8f239d01b0
JeromeStephanie <jeromestephanie0@gmail.com>
2024-05-28 19:06:06 +0100

    Cards Implementation

--- components/feature/SubComponent/Cards.tsx
+++ components/feature/SubComponent/Cards.tsx
@@ -1,10 +1,14 @@
 import { GiVibratingBall } from "react-icons/gi";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
 export default function Cards() {
   return (
-    <div className=" flex justify-center items-center">
-      <div className="border-2 border-white shadow-lg flex gap-[20px] flex-col justify-start p-4 item-center w-[30%]">
-        <div className="flex item-center gap-1 flex-col">
+    <div className="flex mt-[20px] w-full justify-center items-center">
+      <div className="border-2 border-white shadow-lg flex gap-[30px] flex-col justify-start py-[20px] p-4 item-center w-[90%] sm:w-[50%] xl:w-[30%]">
+        <div className="flex justify-center w-[30px] h-[30px] shadow-lg border items-center rounded-full bg-black text-white px-[5px]">
+          <GiVibratingBall size={"2rem"} />
+        </div>
+        <div className="flex item-center gap-[10px] flex-col">
           <p className="text-xl font-semibold">Enterprise</p>
           <p className="text-base font-normal text-primary">
             For large teams and corporations.
@@ -17,6 +21,36 @@ export default function Cards() {
         <button className="bg-black text-base font-normal text-white p-[10px] rounded-lg">
           Get Started
         </button>
+        <div className="w-full h-[20%] text-primary">
+          <hr />
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <p>Features</p>
+          <div className="flex items-center gap-[10px]">
+            <IoIosCheckmarkCircleOutline size={"1.2rem"} />
+            <p className="text-base font-normal text-primary">
+              Advanced employee directory
+            </p>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <IoIosCheckmarkCircleOutline size={"1.2rem"} />
+            <p className="text-base font-normal text-primary">
+              Project management
+            </p>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <IoIosCheckmarkCircleOutline size={"1.2rem"} />
+            <p className="text-base font-normal text-primary">
+              Resource scheduling
+            </p>
+          </div>
+          <div className="flex items-center gap-[10px]">
+            <IoIosCheckmarkCircleOutline size={"1.2rem"} />
+            <p className="text-base font-normal text-primary">
+              Version control
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
