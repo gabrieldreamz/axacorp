243547211d594fefb5dbbfa93f176bb23c06626d
Victor Mbah <mbahvictor16@gmail.com>
2024-05-30 21:16:02 +0100

    Restructured the feature page

--- app/(navigation)/layout.tsx
+++ app/(navigation)/layout.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 export default function FeatureLayout({ children }: { children: JSX.Element }) {
   return (
-    <>
+    <main className="bg-gray-50">
       <MainNavigation />
       {children}
       <Footer />
-    </>
+    </main>
   );
 }
